9cf277710a64db9b20e166b32785eff2a4f74477
vprotsenko <vprotsenko@casgate.io>
2024-07-22 21:49:14 +0300

    [Fraud] Added fraud service

---
+++ fraud/src/main/java/com/microservices/model/FraudCheckHistory.java
@@ -0,0 +1,33 @@
+package com.microservices.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class FraudCheckHistory {
+    @Id
+    @SequenceGenerator(
+        name = "fraud_sequence",
+        sequenceName = "fraud_id_sequence"
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "fraud_id_sequence"
+    )
+    private int id;
+    private Integer customerId;
+    private Boolean isFraudster;
+    private LocalDateTime createdAt;
+}
