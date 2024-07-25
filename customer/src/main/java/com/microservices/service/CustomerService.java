b4db91b22602f200e1a99b7e55244ed8ef14c585
vprotsenko <vprotsenko@casgate.io>
2024-07-25 16:08:04 +0300

    [Customer] Added fraud check logic

--- customer/src/main/java/com/microservices/service/CustomerService.java
+++ customer/src/main/java/com/microservices/service/CustomerService.java
@@ -1,12 +1,20 @@
 package com.microservices.service;
 
+import com.microservices.api.FraudCheckResponse;
 import com.microservices.api.RegisterCustomerRequest;
 import com.microservices.model.Customer;
 import com.microservices.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(
+    CustomerRepository customerRepository,
+    RestTemplate restTemplate
+) {
+
+    public static final String FRAUD_CHECK_ENDPOINT = "/api/v1/fraud-check/";
+    public static final String DEFAULT_API_URL = "http://localhost:8081";
 
     public Customer register(RegisterCustomerRequest request) {
         Customer customer = Customer.builder()
@@ -16,7 +24,18 @@ public record CustomerService(CustomerRepository customerRepository) {
             .phone(request.phone())
             .build();
         // todo: validate request
+        customerRepository.saveAndFlush(customer);
         // todo: check unique
-        return customerRepository.save(customer);
+
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+            DEFAULT_API_URL + FRAUD_CHECK_ENDPOINT + "{customerId}",
+            FraudCheckResponse.class,
+            customer.getId()
+        );
+        if (fraudCheckResponse != null && fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("Fraudster");
+        }
+        // todo: send notification
+        return customer;
     }
 }
--- fraud/src/main/java/com/microservices/controller/FraudController.java
+++ fraud/src/main/java/com/microservices/controller/FraudController.java
@@ -2,15 +2,23 @@ package com.microservices.controller;
 
 import com.microservices.api.FraudCheckResponse;
 import com.microservices.service.FraudCheckService;
-import org.springframework.stereotype.Controller;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public record FraudController(FraudCheckService fraudCheckService) {
+@RestController
+@RequestMapping("api/v1/fraud-check")
+@AllArgsConstructor
+@Slf4j
+public class FraudController {
+    private final FraudCheckService fraudCheckService;
 
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
+        log.info("Fraud check request for customer {}", customerId);
         boolean isFraudster = fraudCheckService.checkCustomer(customerId);
         return new FraudCheckResponse(isFraudster);
     }
