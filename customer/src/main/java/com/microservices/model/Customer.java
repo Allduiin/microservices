bf8ae586926b85e66f868cd0514716709df4d83c
vprotsenko <vprotsenko@casgate.io>
2024-07-29 14:54:16 +0300

    Returned to old spring and java version to make sleuth work correctly

--- customer/src/main/java/com/microservices/model/Customer.java
+++ customer/src/main/java/com/microservices/model/Customer.java
@@ -1,10 +1,10 @@
 package com.microservices.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
